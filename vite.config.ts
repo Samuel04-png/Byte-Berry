7d9abe345bdd419c1ac057e53fe315fd01415a98
NduProject <stackone@nduprojectoutlook.onmicrosoft.com>
2025-11-28 11:29:10 +0200

    Update base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/Byte-Berry/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
 
